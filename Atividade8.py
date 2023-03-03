72cd91aa32733e636d8c2333d565a1bd72ca8505
Rasanhii <107484176+Rasanhii@users.noreply.github.com>
2023-03-03 15:59:56 -0300

    Create Atividade8.py

---
+++ Atividade8.py
@@ -0,0 +1,7 @@
+n = int(input("Digite o número de termos da PA: "))
+a1 = float(input("Digite o valor inicial da PA: "))
+an = float(input("Digite o valor final da PA: "))
+
+som = n * (a1 + an) / 2
+
+print(f"A soma é {som}.")
